60a78a5f0782fb475e2eb2bcfe5b2ab8ee7ee765
ereztdev <erezt.dev@gmail.com>
2020-07-26 19:58:54 -0700

    backend, may segregate to different server. Boiler ready. >>NEXT: bootstrap the grid

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import '../styles/main.scss';
+const App: React.FC = () => {
+    return  <div className={`main--wrapper`}>
+        Hey this is Cool.
+        <div><h1 className={`text-white`}>WOW</h1></div>
+    </div>
+
+};
+
+export default App;
